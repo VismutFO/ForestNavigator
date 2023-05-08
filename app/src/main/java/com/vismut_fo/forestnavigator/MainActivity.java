b34761be3e4e95164744d5ed8b693afdd3d793a3
VismutFO <p3lm2n1@mail.ru>
2023-05-08 12:01:12 +0300

    first working version

--- app/src/main/java/com/vismut_fo/forestnavigator/MainActivity.java
+++ app/src/main/java/com/vismut_fo/forestnavigator/MainActivity.java
@@ -7,23 +7,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.microsoft.maps.CaptureScreenShotListener;
 import com.microsoft.maps.CustomTileMapLayer;
 import com.microsoft.maps.MapCameraChangedEventArgs;
-import com.microsoft.maps.MapLayer;
-import com.microsoft.maps.MapLoadingStatus;
 import com.microsoft.maps.MapRenderMode;
 import com.microsoft.maps.MapStyleSheets;
 import com.microsoft.maps.MapTileBitmapRequestedEventArgs;
 import com.microsoft.maps.MapView;
 import com.microsoft.maps.OnBitmapRequestedListener;
 import com.microsoft.maps.OnMapCameraChangedListener;
-import com.microsoft.maps.OnMapLoadingStatusChangedListener;
 
 import org.pytorch.IValue;
 import org.pytorch.LiteModuleLoader;
@@ -32,12 +37,11 @@ import org.pytorch.Tensor;
 import org.pytorch.torchvision.TensorImageUtils;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.ByteBuffer;
 import java.nio.file.Files;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,113 +49,131 @@ public class MainActivity extends AppCompatActivity {
 
     private Module module;
 
-    private volatile Bitmap result;
-
-    private final Object lock = new Object();
-
-    private int timeNotUsedModule;
+    private volatile Bitmap source, result;
+    private Drawable bitmapDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        timeNotUsedModule = (int) (System.currentTimeMillis()) - 20000;
         setContentView(R.layout.activity_main);
         mMapView = new MapView(this, MapRenderMode.RASTER);  // or use MapRenderMode.RASTER for 2D map
         mMapView.setCredentialsKey(BuildConfig.CREDENTIALS_KEY);
 
-        //CustomTileMapLayer networkResult = new CustomTileMapLayer();
-        result = null;
-        /*
-        networkResult.addOnBitmapRequestedListener(new OnBitmapRequestedListener() {
-            @Override
-            public void onBitmapRequested(@NonNull MapTileBitmapRequestedEventArgs event) {
-                Log.d("<Layers>", "catchBitmap");
-                synchronized (lock) {
-                    synchronized (result) {
-                        if (result == null) {
-                            Log.d("<Layers>", "FaildeToCatchBitmap");
-                            return;
-                        }
-                        int size = result.getRowBytes() * result.getHeight();
-                        ByteBuffer byteBuffer = ByteBuffer.allocate(size);
-                        result.copyPixelsToBuffer(byteBuffer);
-                        event.getRequest().setPixelData(byteBuffer.array());
-                        result = null;
-                    }
-                }
+        source = null;
+
+        result = Bitmap.createBitmap(512, 512, ARGB_8888);
+        for (int i = 0; i < 512; i++) {
+            for (int j = 0; j < 512; j++) {
+                result.setPixel(i, j, Color.TRANSPARENT);
             }
-        });
-        */
-        //mMapView.getLayers().add(networkResult);
+        }
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+        Log.d("<Layers>", Integer.toString(width) + " " + Integer.toString(height));
 
         ((FrameLayout)findViewById(R.id.map_view)).addView(mMapView);
-        mMapView.setMapStyleSheet(MapStyleSheets.aerialWithOverlay());
+        mMapView.setMapStyleSheet(MapStyleSheets.aerial());
 
-        /*
-        mMapView.addOnMapLoadingStatusChangedListener(new OnMapLoadingStatusChangedListener() {
-            @Override
-            public boolean onMapLoadingStatusChanged(MapLoadingStatus mapLoadingStatus) {
-                Log.d("<Layers>", "onMapLoadingStatusChanged");
-                return false;
-            }
-        });
-         */
-
+        TextView latitude = (TextView)findViewById(R.id.XCoordinate), longtitude = (TextView)findViewById(R.id.YCoordinate);
         mMapView.addOnMapCameraChangedListener(new OnMapCameraChangedListener() {
             @Override
             public boolean onMapCameraChanged(@NonNull MapCameraChangedEventArgs mapCameraChangedEventArgs) {
                 Log.d("<Layers>", "onMapCameraChanged");
+                latitude.setText(String.format(Locale.getDefault(), "%f", mMapView.getCenter().getPosition().getLatitude()));
+                longtitude.setText(String.format(Locale.getDefault(), "%f", mMapView.getCenter().getPosition().getLongitude()));
+
+                latitude.invalidate();
+                longtitude.invalidate();
 
                 mMapView.beginCaptureImage(new CaptureScreenShotListener() {
                     @Override
                     public void onCaptureScreenShotCompleted(Bitmap bitmap) {
                         Log.d("<Layers>", "onCaptureScreenShotCompleted");
-                        int temp = (int) (System.currentTimeMillis()) - timeNotUsedModule;
-                        if (temp <= 20000) {
-                            Log.d("<Layers>", Integer.toString(temp));
-                            return;
-                        }
-                        Log.d("<Layers>", "onModuleStart");
-                        timeNotUsedModule = (int) (System.currentTimeMillis());
-                        new Thread(new Runnable() {
-                            final Bitmap bitmapTemp = bitmap;
-                            public void run() {
-                                Bitmap resizedBitmap = Bitmap.createScaledBitmap(
-                                        bitmapTemp, 512, 512, false);
-                                Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(resizedBitmap,
-                                        TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
-                                Log.d("<Layers>", "onNetworkStart");
-                                Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
-                                Log.d("<Layers>", "onNetworkEnd");
-                                float[] scores = outputTensor.getDataAsFloatArray(); // ?
-                                result = Bitmap.createBitmap(512, 512, ARGB_8888);
-                                for (int index = 0; index < 512 * 512; index += 4) {
-                                    byte[] temp = ByteBuffer.allocate(4).putInt(Float.floatToIntBits(scores[index / 4])).array();
-                                    for (int j = 0; j < 4; j++) {
-                                        result.setPixel((index + j) / 512, (index + j) % 512, temp[j]);
-                                    }
-                                }
-                                // display result
-
-                                Log.d("<Layers>", "onModuleEnd");
-                            }
-                            }).start();
-
+                        source = bitmap;
+                        Log.d("<Layers>", "BitmapSize: " + Integer.toString(bitmap.getWidth()) + " " + Integer.toString(bitmap.getHeight()));
                     }
                 });
                 return false;
             }
         });
-
         mMapView.onCreate(savedInstanceState);
+
+        mMapView.getCenter().getPosition();
+
         try {
             module = LiteModuleLoader.load(getModuleFilePath(this, "model3.ptl"));
         } catch (IOException e) {
             Log.d("<Pytorch>", this.getFilesDir().getAbsolutePath(), e);
             finish();
             throw new RuntimeException(this.getFilesDir().getAbsolutePath());
-            //throw new RuntimeException(e);
         }
+
+        bitmapDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(result, width, height, false));
+        ImageView networkResult = findViewById(R.id.networkResult);
+        networkResult.setImageDrawable(bitmapDrawable);
+
+        Button runButton = (Button) findViewById(R.id.runNetworkButton);
+        runButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", "User tapped the runButton");
+                Log.d("<Layers>", "onModuleStart");
+                new Thread(new Runnable() {
+                    final Bitmap bitmapTemp = source;
+                    public void run() {
+
+                        Log.d("<Layers>", "InRun");
+                        Log.d("<Layers>", "InLock");
+                        Log.d("<Layers>", "InResult");
+                        Bitmap resizedBitmap = Bitmap.createScaledBitmap(
+                                bitmapTemp, 512, 512, false);
+                        Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(resizedBitmap,
+                                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+                        Log.d("<Layers>", "onNetworkStart");
+                        Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+                        Log.d("<Layers>", "onNetworkEnd");
+                        float[] scores = outputTensor.getDataAsFloatArray(); // ?
+                        Log.d("<Layers>", "turnedTensorToArray");
+                        for (int i = 0; i < 512; i++) {
+                            for (int j = 0; j < 512; j++) {
+                                if (scores[i * 512 + j] >= scores[262144 + i * 512 + j]) {
+                                    result.setPixel(i, j, 0xA000AA00);
+                                } else {
+                                    result.setPixel(i, j, Color.TRANSPARENT);
+                                }
+
+                            }
+                        }
+                        Log.d("<Layers>", "filledNewResult");
+                        bitmapDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(result, width, height, false));
+                        networkResult.setImageDrawable(bitmapDrawable);
+                        networkResult.invalidate();
+
+                        Log.d("<Layers>", "onModuleEnd");
+
+                    }
+                }).start();
+
+            }
+        });
+
+        Button clearButton = (Button) findViewById(R.id.clearLayerButton);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", "User tapped the clearButton");
+                for (int i = 0; i < result.getWidth(); i++) {
+                    for (int j = 0; j < result.getWidth(); j++) {
+                        result.setPixel(i, j, Color.TRANSPARENT);
+                    }
+                }
+                bitmapDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(result, width, height, false));
+                networkResult.setImageDrawable(bitmapDrawable);
+                networkResult.invalidate();
+            }
+        });
 
     }
 
@@ -165,7 +187,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         mMapView.onResume();
-        timeNotUsedModule++;
     }
 
     @Override
@@ -215,9 +236,5 @@ public class MainActivity extends AppCompatActivity {
             }
             return file.getAbsolutePath();
         }
-    }
-
-    public void runModule(Bitmap bitmap) {
-
     }
 }
