5540c83c9bc28b09bde10bb334a4edfe7abd15ec
VismutFO <p3lm2n1@mail.ru>
2023-05-09 20:18:44 +0300

    add user location and loading spinner

--- app/src/main/java/com/vismut_fo/forestnavigator/MainActivity.java
+++ app/src/main/java/com/vismut_fo/forestnavigator/MainActivity.java
@@ -4,30 +4,36 @@ import static android.graphics.Bitmap.Config.ARGB_8888;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.microsoft.maps.CaptureScreenShotListener;
-import com.microsoft.maps.CustomTileMapLayer;
+import com.microsoft.maps.GPSMapLocationProvider;
 import com.microsoft.maps.MapCameraChangedEventArgs;
+import com.microsoft.maps.MapElementLayer;
 import com.microsoft.maps.MapRenderMode;
 import com.microsoft.maps.MapStyleSheets;
-import com.microsoft.maps.MapTileBitmapRequestedEventArgs;
+import com.microsoft.maps.MapUserLocation;
+import com.microsoft.maps.MapUserLocationTrackingMode;
+import com.microsoft.maps.MapUserLocationTrackingState;
 import com.microsoft.maps.MapView;
-import com.microsoft.maps.OnBitmapRequestedListener;
 import com.microsoft.maps.OnMapCameraChangedListener;
 
 import org.pytorch.IValue;
@@ -37,13 +43,21 @@ import org.pytorch.Tensor;
 import org.pytorch.torchvision.TensorImageUtils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final float[] NO_MEAN_RGB = new float[] {0.5f, 0.5f, 0.5f};
+    private static final float[] NO_STD_RGB = new float[] {0.5f, 0.5f, 0.5f};
+
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
     private MapView mMapView;
 
@@ -51,130 +65,24 @@ public class MainActivity extends AppCompatActivity {
 
     private volatile Bitmap source, result;
     private Drawable bitmapDrawable;
+    private int width, height;
+
+    private TextView latitude, longitude;
+    private Button runButton, clearButton;
+    private ImageView networkResult;
+
+    private ProgressBar spinner;
+
+    private boolean needToDisplayUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMapView = new MapView(this, MapRenderMode.RASTER);  // or use MapRenderMode.RASTER for 2D map
-        mMapView.setCredentialsKey(BuildConfig.CREDENTIALS_KEY);
 
-        source = null;
-
-        result = Bitmap.createBitmap(512, 512, ARGB_8888);
-        for (int i = 0; i < 512; i++) {
-            for (int j = 0; j < 512; j++) {
-                result.setPixel(i, j, Color.TRANSPARENT);
-            }
-        }
-
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-        int height = size.y;
-        Log.d("<Layers>", Integer.toString(width) + " " + Integer.toString(height));
-
-        ((FrameLayout)findViewById(R.id.map_view)).addView(mMapView);
-        mMapView.setMapStyleSheet(MapStyleSheets.aerial());
-
-        TextView latitude = (TextView)findViewById(R.id.XCoordinate), longtitude = (TextView)findViewById(R.id.YCoordinate);
-        mMapView.addOnMapCameraChangedListener(new OnMapCameraChangedListener() {
-            @Override
-            public boolean onMapCameraChanged(@NonNull MapCameraChangedEventArgs mapCameraChangedEventArgs) {
-                Log.d("<Layers>", "onMapCameraChanged");
-                latitude.setText(String.format(Locale.getDefault(), "%f", mMapView.getCenter().getPosition().getLatitude()));
-                longtitude.setText(String.format(Locale.getDefault(), "%f", mMapView.getCenter().getPosition().getLongitude()));
-
-                latitude.invalidate();
-                longtitude.invalidate();
-
-                mMapView.beginCaptureImage(new CaptureScreenShotListener() {
-                    @Override
-                    public void onCaptureScreenShotCompleted(Bitmap bitmap) {
-                        Log.d("<Layers>", "onCaptureScreenShotCompleted");
-                        source = bitmap;
-                        Log.d("<Layers>", "BitmapSize: " + Integer.toString(bitmap.getWidth()) + " " + Integer.toString(bitmap.getHeight()));
-                    }
-                });
-                return false;
-            }
-        });
-        mMapView.onCreate(savedInstanceState);
-
-        mMapView.getCenter().getPosition();
-
-        try {
-            module = LiteModuleLoader.load(getModuleFilePath(this, "model3.ptl"));
-        } catch (IOException e) {
-            Log.d("<Pytorch>", this.getFilesDir().getAbsolutePath(), e);
-            finish();
-            throw new RuntimeException(this.getFilesDir().getAbsolutePath());
-        }
-
-        bitmapDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(result, width, height, false));
-        ImageView networkResult = findViewById(R.id.networkResult);
-        networkResult.setImageDrawable(bitmapDrawable);
-
-        Button runButton = (Button) findViewById(R.id.runNetworkButton);
-        runButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d("BUTTONS", "User tapped the runButton");
-                Log.d("<Layers>", "onModuleStart");
-                new Thread(new Runnable() {
-                    final Bitmap bitmapTemp = source;
-                    public void run() {
-
-                        Log.d("<Layers>", "InRun");
-                        Log.d("<Layers>", "InLock");
-                        Log.d("<Layers>", "InResult");
-                        Bitmap resizedBitmap = Bitmap.createScaledBitmap(
-                                bitmapTemp, 512, 512, false);
-                        Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(resizedBitmap,
-                                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
-                        Log.d("<Layers>", "onNetworkStart");
-                        Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
-                        Log.d("<Layers>", "onNetworkEnd");
-                        float[] scores = outputTensor.getDataAsFloatArray(); // ?
-                        Log.d("<Layers>", "turnedTensorToArray");
-                        for (int i = 0; i < 512; i++) {
-                            for (int j = 0; j < 512; j++) {
-                                if (scores[i * 512 + j] >= scores[262144 + i * 512 + j]) {
-                                    result.setPixel(i, j, 0xA000AA00);
-                                } else {
-                                    result.setPixel(i, j, Color.TRANSPARENT);
-                                }
-
-                            }
-                        }
-                        Log.d("<Layers>", "filledNewResult");
-                        bitmapDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(result, width, height, false));
-                        networkResult.setImageDrawable(bitmapDrawable);
-                        networkResult.invalidate();
-
-                        Log.d("<Layers>", "onModuleEnd");
-
-                    }
-                }).start();
-
-            }
-        });
-
-        Button clearButton = (Button) findViewById(R.id.clearLayerButton);
-        clearButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d("BUTTONS", "User tapped the clearButton");
-                for (int i = 0; i < result.getWidth(); i++) {
-                    for (int j = 0; j < result.getWidth(); j++) {
-                        result.setPixel(i, j, Color.TRANSPARENT);
-                    }
-                }
-                bitmapDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(result, width, height, false));
-                networkResult.setImageDrawable(bitmapDrawable);
-                networkResult.invalidate();
-            }
-        });
-
+        initMap(savedInstanceState);
+        initAllForNetwork();
+        initAllGraphics();
     }
 
     @Override
@@ -219,7 +127,201 @@ public class MainActivity extends AppCompatActivity {
         mMapView.onLowMemory();
     }
 
-    public static String getModuleFilePath(Context context, String assetName) throws IOException {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    needToDisplayUser = true;
+                }
+                else {
+                    Log.d("<Layers>", "Something wrong with permission");
+                }
+            }
+
+        }
+    }
+
+    private void initMap(Bundle savedInstanceState) {
+        mMapView = new MapView(this, MapRenderMode.RASTER);  // or use MapRenderMode.RASTER for 2D map
+        mMapView.setCredentialsKey(BuildConfig.CREDENTIALS_KEY);
+
+        ((FrameLayout)findViewById(R.id.map_view)).addView(mMapView);
+        mMapView.setMapStyleSheet(MapStyleSheets.aerial());
+
+        mMapView.addOnMapCameraChangedListener(new OnMapCameraChangedListener() {
+            @Override
+            public boolean onMapCameraChanged(@NonNull MapCameraChangedEventArgs mapCameraChangedEventArgs) {
+                Log.d("<Layers>", "onMapCameraChanged");
+                latitude.setText(String.format(Locale.getDefault(), "%f", mMapView.getCenter().getPosition().getLatitude()));
+                longitude.setText(String.format(Locale.getDefault(), "%f", mMapView.getCenter().getPosition().getLongitude()));
+
+                latitude.invalidate();
+                longitude.invalidate();
+
+                mMapView.beginCaptureImage(new CaptureScreenShotListener() {
+                    @Override
+                    synchronized public void onCaptureScreenShotCompleted(Bitmap bitmap) {
+                        Log.d("<Layers>", "onCaptureScreenShotCompleted");
+                        for (int i = 0; i < result.getWidth(); i++) {
+                            for (int j = 0; j < result.getHeight(); j++) {
+                                result.setPixel(i, j, Color.TRANSPARENT);
+                            }
+                        }
+                        bitmapDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(result, width, height, false));
+                        networkResult.setImageDrawable(bitmapDrawable);
+                        networkResult.invalidate();
+
+                        //source = bitmap.extractAlpha();
+                        source = bitmap;
+                        Log.d("<Layers>", source.getConfig().toString());
+
+                        File file = new File("", "file.jpg");
+                        try (FileOutputStream out = new FileOutputStream("file.jpg")) {
+
+                            //bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                            //MediaStore.Images.Media.insertImage(getContentResolver(),file.getAbsolutePath(),file.getName(),file.getName());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        Log.d("<Layers>", "BitmapSize: " + Integer.toString(bitmap.getWidth()) + " " + Integer.toString(bitmap.getHeight()));
+                    }
+                });
+                return false;
+            }
+        });
+        needToDisplayUser = false;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = new String[]
+                    {android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION};
+            ActivityCompat.requestPermissions(this, permissions, MY_PERMISSIONS_REQUEST_LOCATION);
+        }
+        else {
+            needToDisplayUser = true;
+        }
+        if (needToDisplayUser) {
+            Log.d("<Layers>", "getPermission");
+            MapUserLocation userLocation = mMapView.getUserLocation();
+
+            MapUserLocationTrackingState userLocationTrackingState =
+                    userLocation.startTracking(new GPSMapLocationProvider.Builder(getApplicationContext()).build());
+            if (userLocationTrackingState == MapUserLocationTrackingState.PERMISSION_DENIED) {
+                Log.d("<Layers>", "Get to user location without permission");
+            } else if (userLocationTrackingState == MapUserLocationTrackingState.READY) {
+                userLocation.setTrackingMode(MapUserLocationTrackingMode.CENTERED_ON_USER);
+            } else if (userLocationTrackingState == MapUserLocationTrackingState.DISABLED) {
+                Log.d("<Layers>", "Need to check on line 191");
+            }
+        }
+        mMapView.onCreate(savedInstanceState);
+    }
+
+    private void initAllForNetwork() {
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        width = size.x;
+        height = size.y;
+        Log.d("<Layers>", Integer.toString(width) + " " + Integer.toString(height));
+
+        source = null;
+        result = Bitmap.createBitmap(512, 512, ARGB_8888);
+        for (int i = 0; i < 512; i++) {
+            for (int j = 0; j < 512; j++) {
+                result.setPixel(i, j, Color.TRANSPARENT);
+            }
+        }
+
+        try {
+            module = LiteModuleLoader.load(getModuleFilePath(this, "model.ptl"));
+        } catch (IOException e) {
+            Log.d("<Pytorch>", this.getFilesDir().getAbsolutePath(), e);
+            finish();
+            throw new RuntimeException(this.getFilesDir().getAbsolutePath());
+        }
+    }
+
+    private void initAllGraphics() {
+        spinner = (ProgressBar)findViewById(R.id.progressSpinner);
+        spinner.setVisibility(View.GONE);
+
+        latitude = (TextView)findViewById(R.id.XCoordinate);
+        longitude = (TextView)findViewById(R.id.YCoordinate);
+
+        bitmapDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(result, width, height, false));
+        networkResult = findViewById(R.id.networkResult);
+        networkResult.setImageDrawable(bitmapDrawable);
+
+        runButton = (Button) findViewById(R.id.runNetworkButton);
+        runButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", "User tapped the runButton");
+                Log.d("<Layers>", "onModuleStart");
+                spinner.setVisibility(View.VISIBLE);
+                new Thread(new Runnable() {
+                    final Bitmap bitmapTemp = source;
+                    synchronized public void run() {
+
+                        Log.d("<Layers>", "InRun");
+                        Log.d("<Layers>", "InLock");
+                        Log.d("<Layers>", "InResult");
+                        Bitmap resizedBitmap = Bitmap.createScaledBitmap(
+                                bitmapTemp, 512, 512, false);
+                        Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(resizedBitmap,
+                                NO_MEAN_RGB, NO_STD_RGB);
+
+                        Log.d("<Layers>", "onNetworkStart");
+                        //Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+                        Map<String, IValue> outTensors = module.forward(IValue.from(inputTensor)).toDictStringKey();
+                        final Tensor outputTensor = Objects.requireNonNull(outTensors.get("out")).toTensor();
+
+                        Log.d("<Layers>", "onNetworkEnd");
+                        float[] scores = outputTensor.getDataAsFloatArray(); // ?
+                        Log.d("<Layers>", "scores length: " + Integer.toString(scores.length));
+                        Log.d("<Layers>", "turnedTensorToArray");
+                        for (int i = 0; i < 512; i++) {
+                            for (int j = 0; j < 512; j++) {
+                                if (scores[i * 512 + j] < scores[262144 + i * 512 + j]) {
+                                    result.setPixel(i, j, 0xA000AA00);
+                                } else {
+                                    result.setPixel(i, j, Color.TRANSPARENT);
+                                }
+
+                            }
+                        }
+                        Log.d("<Layers>", "filledNewResult");
+                        spinner.setVisibility(View.INVISIBLE);
+                        bitmapDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(result, width, height, false));
+                        networkResult.setImageDrawable(bitmapDrawable);
+                        networkResult.invalidate();
+
+                        Log.d("<Layers>", "onModuleEnd");
+
+                    }
+                }).start();
+
+            }
+        });
+
+        clearButton = (Button) findViewById(R.id.clearLayerButton);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", "User tapped the clearButton");
+                for (int i = 0; i < result.getWidth(); i++) {
+                    for (int j = 0; j < result.getWidth(); j++) {
+                        result.setPixel(i, j, Color.TRANSPARENT);
+                    }
+                }
+                bitmapDrawable = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(result, width, height, false));
+                networkResult.setImageDrawable(bitmapDrawable);
+                networkResult.invalidate();
+            }
+        });
+    }
+
+    private static String getModuleFilePath(Context context, String assetName) throws IOException {
         File file = new File(context.getFilesDir(), assetName);
         if (file.exists() && file.length() > 0) {
             return file.getAbsolutePath();
